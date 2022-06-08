10eb5e0e10474f242a8c4870c3c1a264e8c021c3
nischal nandurkar <nischalnischal6@gmail.com>
2022-06-08 11:08:02 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { Table } = require("./modules/table");
+const cors = require('cors');
 const app = express();
 const PORT = 4000;
+app.use(cors());
 app.use(express.json());
 
 const DB =
